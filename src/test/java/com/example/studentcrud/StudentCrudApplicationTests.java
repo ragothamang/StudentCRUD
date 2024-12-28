99ee8ee6ffc3685c70cee43c9b6609e05d267647
ragogiritech@gmail.com <ragogiritech@gmail.com>
2024-12-28 13:37:17 +0530

    Mock service - adding MockBean for service

--- src/test/java/com/example/studentcrud/StudentCrudApplicationTests.java
+++ src/test/java/com/example/studentcrud/StudentCrudApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.studentcrud;
 
+import com.example.studentcrud.controllers.StudentController;
+import com.example.studentcrud.dtos.StudentDTO;
+import com.example.studentcrud.models.Student;
+import com.example.studentcrud.services.IStudentService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class StudentCrudApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private StudentController studentcontroller;
+
+   @MockitoBean
+    private IStudentService studentService;
+@Test
+   public void Test_GetStudent_WithValid_Id_Runs_Successfully(){
+    int studId = 2;
+
+    //arrange
+    when(studentService.getStudentById(studId)).thenReturn(new Student());
+
+    //act
+    ResponseEntity<StudentDTO> studentDTOResponseEntity =
+            studentcontroller.getStudentById(studId);
+
+    assertNotNull(studentDTOResponseEntity);
+    assertNotNull(studentDTOResponseEntity.getBody());
+
+    //assert
+
+   }
 
 }
